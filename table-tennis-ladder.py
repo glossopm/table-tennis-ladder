09ac6312e9f0cfc767d6af99f100afa642c5f75e
e-birch <43006551+e-birch@users.noreply.github.com>
2018-09-12 12:05:59 +0100

    Update table-tennis-ladder.py
    new multiple leaderboards functionality added (except for in "record match" section)

--- table-tennis-ladder.py
+++ table-tennis-ladder.py
@@ -1,5 +1,27 @@
 import sys
 from prettytable import PrettyTable
+import csv
+
+
+# read leaderboards order data from .txt file
+def get_leaderboards(filename):
+    leaderboards_file = open(filename, "r")
+    lboards_str = leaderboards_file.read()
+    lboards = lboards_str.split(",")
+    leaderboards_file.close()
+    return lboards
+
+
+# write leaderboards order data to .txt file
+def write_lboards(filename, lboardsOrder):
+    lboards_file = open(filename, "w")
+    write_str = ""
+    for i in lboardsOrder:
+        write_str = write_str + i + ","
+    write_str = write_str.rstrip(",").lstrip(",")
+    lboards_file.write(write_str)
+    lboards_file.close()
+    return
 
 
 # read players data from .txt file, store and return "players" list
@@ -23,6 +45,19 @@ def write_players(filename, players):
     return
 
 
+# read leaderboards data from .csv file and store and return "leaderboards" dictionary
+def get_lboards_dict(filename):
+
+    reader = csv.reader(open(filename, "r"))
+    mydict = {}
+    for row in reader:
+        key, val = row
+        mydict[str(key)] = val
+
+    #print mydict
+    return mydict
+
+
 # read ladder data from .txt file, store and return "ladder" list
 def get_ladder(filename):
     ladder_file = open(filename, "r")
@@ -32,16 +67,11 @@ def get_ladder(filename):
     return ladder
 
 
-# write ladder data from "ladder" list to .txt file
-def write_ladder(filename, ladder):
-    ladder_file = open(filename, "w")
-    write_str = ""
-    for i in ladder:
-        write_str = write_str + i + ","
-    write_str = write_str.rstrip(",").lstrip(",")
-    ladder_file.write(write_str)
-    ladder_file.close()
-    return
+def write_lboards_dict(filename, lboard_dict):
+
+    w = csv.writer(open(filename, "w"))
+    for key, val in lboard_dict.items():
+        w.writerow([key, val])
 
 
 # moves or adds winner/loser to correct positions in ladder
@@ -94,22 +124,28 @@ def view_players(players):
 # record new match (straight from command line)
 def add_new_matches_list(matches, players, ladder):
     failed_records = []
+    duplicates = []
     for i in range(0, len(matches)-1, 2):
-        winner = matches[i]
-        loser = matches[i+1]
+        winner = matches[i].rstrip(",").lstrip(",")
+        loser = matches[i+1].rstrip(",").lstrip(",")
 
-        if winner not in players or (winner.lower() not in [i.lower() for i in players]):
-            failed_records.append((winner, loser))
-        elif loser not in players or (loser.lower() not in [i.lower() for i in players]):
-            failed_records.append((winner, loser))
+        if winner == loser:
+            duplicates.append(winner)
         else:
-            ladder = match_played(winner, loser, ladder)
-            print "Match: " + str(winner) + " beating " + str(loser) + " has been recorded."
+            if winner not in players or (winner.lower() not in [i.lower() for i in players]):
+                failed_records.append((winner, loser))
+            elif loser not in players or (loser.lower() not in [i.lower() for i in players]):
+                failed_records.append((winner, loser))
+            else:
+                ladder = match_played(winner, loser, ladder)
+                print "Match: " + str(winner) + " beating " + str(loser) + " has been recorded."
     write_ladder("ladder.txt", ladder)
 
     for i in failed_records:
         print "Match: " + str(i[0]) + " beating " + str(i[1]) + " not recorded - one or both players missing from players list."
 
+    for i in duplicates:
+        print "Match for player " + str(i) + " not recorded - players cannot play against themselves"
     return ladder
 
 
@@ -141,6 +177,10 @@ def enter_matches(players, ladder):
         else:
             break
 
+    if winner == loser:
+        print "Players' names must be unique. Please try again."
+        enter_matches(players, ladder)
+
     new_ladder = match_played(winner, loser, ladder)
     write_ladder("ladder.txt", new_ladder)
 
@@ -159,19 +199,25 @@ def enter_matches(players, ladder):
 def add_new_players_list(players, new_players):
     duplicate_players = []
     added_players = []
+    failed_players = []
 
     for player_name in new_players:
         if player_name in players or player_name.lower() in [i.lower() for i in players]:
             duplicate_players.append(player_name)
         else:
-            players.append(player_name)
-            added_players.append(player_name)
+            if not player_name.isalpha():
+                failed_players.append(player_name)
+            else:
+                players.append(player_name)
+                added_players.append(player_name)
     write_players("players.txt", players)
 
     if len(added_players) != 0:
         print "The following players were added successfully: " + ", ".join(added_players)
     if len(duplicate_players) != 0:
         print "The following players are already in the players list: " + ", ".join(duplicate_players)
+    if len(failed_players) != 0:
+        print "Adding the following players failed - please check names and try again: " + ", ".join(failed_players)
 
 
 # add new players (from menu option)
@@ -189,6 +235,38 @@ def menu_add_players(players):
             write_players("players.txt", players)
             print ""
             main_menu()
+
+
+def change_lboard(lboardsDict, lboardsOrder, new_name):
+    print "change function"
+    print new_name
+    if new_name not in lboardsOrder:
+        lboardsOrder.insert(0, new_name)
+        print "The leaderboard '" + str(new_name) + " has been created, and is now the current leaderboard."
+        lboardsDict[new_name] = []
+    else:
+        # removes new_name from list wherever it is then adds it to the front to become the default leaderboard
+        del lboardsOrder[lboardsOrder.index(new_name)]
+        lboardsOrder.insert(0, new_name)
+        print "The current leaderboard is now '" + new_name + "'."
+
+    write_lboards("leaderboardsOrder.txt", lboardsOrder)
+    write_lboards_dict("leaderboards.csv", lboardsDict)
+
+
+def change_lboard_menu(lboardsDict, lboardsOrder):
+    new_name = str(raw_input("Please enter a leaderboard: "))
+    if new_name not in lboardsOrder:
+        lboardsOrder.insert(0, new_name)
+        print "The leaderboard '" + new_name + "' has been created, and is now the current leaderboard."
+        lboardsDict[new_name] = []
+    else:
+        del lboardsOrder[lboardsOrder.index(new_name)]
+        lboardsOrder.insert(0, new_name)
+        print "The current leaderboard is now '" + new_name + "'."
+
+    write_lboards("leaderboardsOrder.txt", lboardsOrder)
+    write_lboards_dict("leaderboards.csv", lboardsDict)
 
 
 # search function returns position of player
@@ -219,7 +297,11 @@ def search_players_menu(ladder):
 def main_menu():
     # re-read the players/ladders data - in essence, do a "refresh"
     players = get_players("players.txt")
-    ladder = get_ladder("ladder.txt")
+    lboards_order = get_leaderboards("leaderboardsOrder.txt")
+    lboards_dict = get_lboards_dict(("leaderboards.csv"))
+
+    ladder = lboards_dict[lboards_order[0]]
+
     print ""
     print "-- Menu --"
     print "1) Add players"
@@ -264,6 +346,7 @@ def print_help():
           "[] \t\t\t\t Not specifying an argument will bring up the main menu.\n"
 
 
+# main - entry point of program
 def main():
 
     # retrieve non-script arguments
@@ -273,8 +356,9 @@ def main():
     if not args:
         main_menu()
     else:
+        lboardOrder = get_leaderboards("leaderboardsOrder.txt")
         players = get_players("players.txt")
-        ladder = get_ladder("ladder.txt")
+        lboards_dict = get_lboards_dict("leaderboards.csv")
 
         # "--add" argument specified
         if args[0] == "--add":
@@ -303,12 +387,28 @@ def main():
 
         # "--view" argument specified
         elif args[0] == "--view":
-            # send user to leaderboard view
-            view_leaderboard(ladder)
+            # send user to default leaderboard view
+            if len(args) == 1:
+                view_leaderboard(lboards_dict[lboardOrder[0]])
+            else:
+                view_leaderboard(lboards_dict[args[1]])
+
+        elif args[0] == "--list":
+            print "Current existing leaderboards: " + ",".join(lboardOrder) + "."
+            print "The default leaderboard is currently '" + lboardOrder[0] + "."
+
 
         elif args[0] == "--players":
             # send user to players view
             view_players(players)
+
+        elif args[0] == "--change":
+            lboard_name = args[1:]
+            if len(lboard_name) != 0:
+                lboard_name = str(args[1])
+                change_lboard(lboards_dict, lboardOrder, lboard_name)
+            else:
+                change_lboard_menu(lboards_dict, lboardOrder)
 
         # "--help" argument specified
         elif args[0] == "--help":
