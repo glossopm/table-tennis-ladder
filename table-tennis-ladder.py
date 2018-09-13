8c87dd6269dcff046a9ab1ae11e509fd776cc2af
Malik Glossop <malik.glossop@infinityworks.com>
2018-09-13 11:22:02 +0100

    Added in lboards_html.html to view lboards online

--- table-tennis-ladder.py
+++ table-tennis-ladder.py
@@ -87,27 +87,42 @@ def create_html_file(lboards_dict):
 
     html_string = ""
 
-    html_string = "<!DOCTYPE html>\n"
-    html_string = html_string + "<html>\n<head>\n"
-    html_string = html_string + "<title>---VIEW LEADERBOARDS---</title>\n</head>\n"
+    html_string = "<!DOCTYPE html><br>"
+    html_string = html_string + """<html>\n<head>\n<style>
+    body {
+      background-color: powderblue;
+    }
+    h1  {
+      color: blue;
+    }
+    table, th, td {
+    border: 1px solid black;
+    text-align: center;
+    border-spacing: 5px;
+    }
+    p  {
+      color: red;
+    }
+   </style>"""
+    html_string = html_string + "<title>---VIEW LEADERBOARDS---</title>\n</head>\n <h1> Table-Tennis Leaderboard </h1>"
     html_string = html_string + "<body>\n"
 
     for i in lboards_dict:
         table = PrettyTable()
         table.field_names = ["Ranking", "Name"]
         #print i
-        html_string = html_string + i + "\n"
+        html_string = html_string + i + "<br>"
 
         for j in lboards_dict[i]:
             #print j
             table.add_row([str(lboards_dict[i].index(j)+1), str(j)])
 
-        html_string = html_string + str(table)
-        html_string = html_string + "\n\n"
+        html_string = html_string + "<table border: 1px solid black>" +table.get_html_string() +"</table>"
+        html_string = html_string + "<br><br>"
 
     html_string = html_string + "</body>\n"
     html_string = html_string + "</html>"
-    html_file = open("lboards_html.index", "w+")
+    html_file = open("lboards_html.html", "w+")
 
     html_file.write(html_string)
     html_file.close()
