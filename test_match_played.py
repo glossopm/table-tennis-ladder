332d3ae511ef8ae90fb9865359709ebb232213b0
Malik Glossop <malik.glossop@infinityworks.com>
2018-09-17 16:06:37 +0100

    Edited the get_lboards to write a string rather than list which fixes the --change argument

--- test_match_played.py
+++ test_match_played.py
@@ -4,14 +4,43 @@ from table_tennis_ladder import *
 
 class TestMatchPlayed(unittest.TestCase):
     
-    def match_winner_higher(self):
+    def test_match_winner_higher(self):
         winner = "Alpha"
         loser = "Beta"
         ladder = ["Gamma", "Alpha", "Beta", "Delta"]
-
         new_ladder = match_played(winner, loser, ladder)
         self.assertEqual("Alpha", new_ladder[1])
 
+    def test_match_loser_higher(self):
+        winner = "Beta"
+        loser = "Alpha"
+        ladder = ["Gamma", "Alpha", "Beta", "Delta"]
+        new_ladder = match_played(winner, loser, ladder)
+        self.assertEqual("Beta", new_ladder[1])
+
+    def test_match_loser_not_listed(self):
+        winner = "Beta"
+        loser = "Alpha"
+        ladder = ["Gamma", "Beta", "Delta"]
+        new_ladder = match_played(winner, loser, ladder)
+        self.assertEqual("Alpha", new_ladder[3])
+
+    def test_match_winner_not_listed(self):
+        winner = "Alpha"
+        loser = "Beta"
+        ladder = ["Gamma", "Beta", "Delta"]
+        new_ladder = match_played(winner, loser, ladder)
+        self.assertEqual("Alpha", new_ladder[1])
+        self.assertEqual("Beta", new_ladder[2])
+
+    def test_match_both_not_listed(self):
+        winner = "Alpha"
+        loser = "Beta"
+        ladder = ["Gamma", "Sigma" "Delta"]
+        new_ladder = match_played(winner, loser, ladder)
+        print new_ladder
+        self.assertEqual("Alpha", new_ladder[3])
+        self.assertEqual("Beta", new_ladder[4])
 
 if __name__ == '__main__':
     unittest.main()
